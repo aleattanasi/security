ba6f3b0635fd67d254a66663f50fd1963e497b44
aleattanasi <36075895+aleattanasi@users.noreply.github.com>
2020-10-13 16:56:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ def say_hello():
     return "HO ATTIVATO PUB/SUB"
 
 
+@app.route('/a', methods=['GET'])
+def say_hello():
+    return "HO ATTIVATO PUB/SUB MA SOLO SE TO CRIR"
+
+@app.route('/c', methods=['GET'])
+def say_hello():
+    return "HO ATTIVATO PUB/SUB MA SOLO SE TO CRIR pero stavot ha prmut c"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
