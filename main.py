f461ab38e94674300df29d5401e54a3fa64adb0f
aleattanasi <36075895+aleattanasi@users.noreply.github.com>
2020-10-31 10:13:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ def evenZt():
 if __name__ == '__main__':
 	# This is used when running locally. Gunicorn is used to run the
 	# application on Google App Engine. See entrypoint in app.yaml.
-	app.run(host='0.0.0.0', port=80, debug=True)
+	app.run(host='0.0.0.0', port=8080, debug=True)
 # [END gae_flex_storage_app]
