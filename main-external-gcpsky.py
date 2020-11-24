095ad65da8001108603802704296adab243b4757
aleattanasi <36075895+aleattanasi@users.noreply.github.com>
2020-11-24 15:11:27 +0100

    Update main-external-gcpsky.py

--- main-external-gcpsky.py
+++ main-external-gcpsky.py
@@ -1,79 +1,76 @@
-
 from flask import Flask, request, json
-import datetime;
+import datetime
 from google.cloud import pubsub_v1
 
 app = Flask(__name__)
 
 #funzione per l'invio di dati a pubsub
 def toPubSub(data):
-	project_id = "sky-it-tech-itv-myaccount-dev"
-	topic_id = "analytics-topic"
+    project_id = "sky-it-tech-itv-myaccount-dev"
+    topic_id = "analytics-topic"
 
-	publisher = pubsub_v1.PublisherClient()
-	topic_path = publisher.topic_path(project_id, topic_id)
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
 
-	# When you publish a message, the client returns a future.
-	da = json.dumps(data)
-	future = publisher.publish(topic_path, da.encode('utf-8'))
+    # When you publish a message, the client returns a future.
+    da = json.dumps(data)
+    future = publisher.publish(topic_path, da.encode('utf-8'))
+
+#funzione per calcolare l'id della sessione
+def sessiId(client_id):
+    currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    client = client_id
+    session = str(abs((hash(client))))
+    session_id = session+'_'+currentTime
+    return session_id
 
 @app.route('/')
 def index():
-	return """
+    return """
 <form method="POST" action="/addResource" enctype="multipart/form-data"
   <label for="fname">IL SERVIZIO SUL GRUPPO DI ISTANZE SUPERVISIONATO E' ONLINE</label><br>
   <br>
 </form>
 """
 
-@app.route('/sessionBegin', methods=['POST'])
-#server-istances group
-def begin():
-	event_data = request.data
-	data = json.loads(event_data)
-	cl_id = data["client_id"]
-	event_timest = data["device_info"]["event_ts"]
-	session_id = sessiId(cl_id)
-	#todo svirgolettare session_id valore
-	#todo gestire bene timestamp, su tm si chiama msg_ts
-	messaggio = {"properties":{"event_name":"SESSION_BEGIN", "session_id": "session_id", "client_id": "client_id", "device_info":data["device_info"]},"info":{}, "opt_info":{}}
-	toPubSub(messaggio)
-
-	toClient = {"session_id" : "session_id", "format" : "JSONorMP"}
-	b=json.dumps(toClient)
-	return b
-
-#funzione per calcolare l'id della sessione
-def sessiId(client_id):
-	currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	client = client_id
-	session = str(abs((hash(client))))
-	session_id = session+'_'+currentTime
-	return session_id
 
 
 @app.route('/eventFrame', methods=['POST'])
 #server-istances group
 def event():
-	#qui mi arriva o una lista di eventi o un evento e mando dirett a pubsub
-	event_data = request.data
-	data = json.loads(event_data)
-	toPubSub(data)
-	return "frame sent"
+    event_data = request.data
+    data = json.loads(event_data)
 
-@app.route('/sessionEnd', methods=['POST'])
-#server-istances group
-def end():
-	event_data = request.data
-	data = json.loads(event_data)
-	timest = data["event_timestamp"]
-	risp ={"properties":{"event_name":"SESSION_END", "session_id": "session_id", "client_id": "client_id", "event_timestamp":timest}, "info":{}, "opt_info":{}}
-	toPubSub(risp)
-	b=json.dumps(risp)
-	return b
+    if "device_info" in data:
+        #sessionStart
+        cl_id = data["client_id"]
+        event_timest = data["device_info"]["event_ts"]
+        session_id = sessiId(cl_id)
+        # todo svirgolettare session_id valore
+        # todo gestire bene timestamp, su tm si chiama msg_ts
+        messaggio = {"properties": {"event_name": "SESSION_BEGIN", "session_id": "session_id", "client_id": "client_id", "event_timestamp":data["device_info"]["msg_ts"],
+                                    "device_info": data["device_info"]}, "info": {}, "opt_info": {}}
+        toPubSub(messaggio)
+        toClient = {"session_id": "session_id", "format": "JSONorMP"}
+        b = json.dumps(toClient)
+        return b
+    elif "session_id" in data:
+        #sessionend
+        timest = data["event_timestamp"]
+        risp = {"properties": {"event_name": "SESSION_END", "session_id": "session_id", "client_id": "client_id",
+                     "event_timestamp": timest}, "info": {}, "opt_info": {}}
+        toPubSub(risp)
+        b = json.dumps(risp)
+        return b
+    else:
+        # evtframe
+        for x in data:
+            x["properties"]["app_code"] = "XXCXCX"
+            toPubSub(x)
+        return "frame sent"
 
 if __name__ == '__main__':
 	# This is used when running locally. Gunicorn is used to run the
 	# application on Google App Engine. See entrypoint in app.yaml.
-	app.run(host='0.0.0.0', port=8080, debug=True)
+	app.run(host='0.0.0.0', port=80, debug=True)
 # [END gae_flex_storage_app]
